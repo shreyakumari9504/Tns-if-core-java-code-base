3fb3f7c1d41774fa150c17cf4b56dfa1dc074916
shreya kumari <shreyakumari9504ssss@gmail.com>
2025-07-10 17:03:47 +0530

    this is day 10 project

---
+++ day-10/src/tnsif/multiplecatchdemo/multiplecatchdemo.java
@@ -0,0 +1,13 @@
+package tnsif.multiplecatchdemo;
+
+public class multiplecatchdemo {
+public static void main(String[] args) {
+		
+		System.out.println("I am in main method ");
+		
+		Division.divide();
+
+	}
+	
+
+}
